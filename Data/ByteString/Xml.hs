15b96211e4d1939047b572c34d9b1b03c9745d0c
Pepe Iborra <pepeiborra@gmail.com>
2016-12-12 17:33:11 +0000

    fix tests

--- Data/ByteString/Xml.hs
+++ Data/ByteString/Xml.hs
@@ -7,25 +7,15 @@ module Data.ByteString.Xml
   , ErrorType(..)
   , parse
   , children
-  , childrenBy
-  , attributeBy
-  , location
   ) where
 
 import Data.ByteString.Xml.Internal
 import Data.ByteString.Xml.Types
-import qualified Data.ByteString.Char8 as BS
-import Data.Monoid
+import qualified Data.ByteString as BS
 import Control.Lens
-import Control.Monad
 
-childrenBy :: Node -> Str -> [Node]
-childrenBy p s = filter (\n -> name n == s) $ children p
-
-attributeBy :: Node -> Str -> Maybe Attribute
-attributeBy n s = getAlt $ foldMap (\a -> Alt(a <$ guard(s == nameA a))) $ attributes n
 --  listToMaybe $ filter (\(Attribute a _) -> a == s) $ attributes n
-
+{-
 location :: Document -> Node -> (Int, Int)
 location Doc{ptr=src} n@(Node _ s _ _) =
   BS.foldl' f (pair 1 1) $ BS.take (fromIntegral s) src
@@ -36,3 +26,5 @@ location Doc{ptr=src} n@(Node _ s _ _) =
             | c == '\n' = pair (line+1) 1
             | c == '\t' = pair line (col+8)
             | otherwise = pair line (col+1)
+
+-}
